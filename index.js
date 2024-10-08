c030dee929fe99b11b7e043bf3412b990219736f
Riku Lauttia <riku.lauttia@gmail.com>
2024-10-08 15:42:46 +0300

    edistyy

--- index.js
+++ index.js
@@ -99,7 +99,7 @@ app.get("/info", (request, response) => {
   response.send(info);
 });
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
